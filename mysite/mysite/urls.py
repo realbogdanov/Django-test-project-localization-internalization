a7d943f535649fb1cc41cd13b80bf33dcfc693c6
Vasilii Bogdanov <vasiliibogdanov@MacBook-Pro-Vasilii.local>
2024-01-28 21:17:44 +0300

    Add project with localization and internalization work

---
+++ mysite/mysite/urls.py
@@ -0,0 +1,23 @@
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import path, include
+from django.conf.urls.i18n import i18n_patterns
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+]
+
+urlpatterns += i18n_patterns(
+        path('myauth/', include('myauth.urls')),
+        path('shopapp/', include('shopapp.urls')),
+)
+
+if settings.DEBUG:
+    urlpatterns.extend(
+        static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    )
+
+    urlpatterns.extend(
+        static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    )
